fdd9f8aade3d73c1bd9cca20061c28278802b1d5
sjn <sujen@bolevard.com>
2022-05-30 23:14:35 -0400

    separate data structure for lock and staking

--- src/main/Vault.sol
+++ src/main/Vault.sol
@@ -7,14 +7,15 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Vault is ReentrancyGuard {
 
-    struct User {
-        uint256 stakedAmount;
-        // uint256 rewardsClaimedAmount;
-        uint256 lockedAtTimestamp;
+    struct UserTokenLock {
         uint256 lockedAmount;
+        uint256 lockedAtTimestamp;
+        uint256 claimedAmount;
     }
 
-    mapping(address => User) public users;
+    mapping(address => UserTokenLock) public userTokenLocks;
+
+    mapping(address => uint256) public userStakeAmountMap;
 
     address public immutable cardTokenAddr;
 
@@ -23,6 +24,8 @@ contract Vault is ReentrancyGuard {
     event Staked(address indexed _staker, uint256 _amount, uint256 _actualAmount, uint256 _newStakedAmount);
     event Debug(string text);
     event Unstaked(address _requestedBy, address indexed _staker, uint256 _amount, uint256 _newStakedAmount);
+    event Locked(address _locker, uint256 _amount, uint256 _actualAmount);
+    event ClaimedUnlocked(address _claimer, uint256 _amount, uint monthsElapsed, uint256 _totalClaimed);
 
     constructor(
         address _cardTokenAddr
@@ -57,19 +60,14 @@ contract Vault is ReentrancyGuard {
         // sync state
         _sync();
 
-        // add an entry for the user
-        User storage user = users[address(msg.sender)];
-
-        require(user.lockedAtTimestamp <= 0, "Cant stake more IF you have locked");
-
         uint256 prevVaultTokenBalance = IERC20(cardTokenAddr).balanceOf(address(this));
         SafeERC20.safeTransferFrom(IERC20(cardTokenAddr), address(msg.sender), address(this), _amount);
         uint256 newVaultTokenBalance = IERC20(cardTokenAddr).balanceOf(address(this));
         uint256 addedAmount = newVaultTokenBalance - prevVaultTokenBalance;
 
-        user.stakedAmount += addedAmount;
+        userStakeAmountMap[msg.sender] += addedAmount;
 
-        emit Staked(msg.sender, _amount, addedAmount, user.stakedAmount);
+        emit Staked(msg.sender, _amount, addedAmount, userStakeAmountMap[msg.sender]);
     }
 
 
@@ -79,26 +77,55 @@ contract Vault is ReentrancyGuard {
     ) external  {
         require(_amount > 0, "requires amount > 0");
 
-        User storage user = users[_stakerAddr];
+        require(userStakeAmountMap[_stakerAddr] > 0, "no staked balance to unstake");
 
-        require(user.stakedAmount > 0, "no staked balance to unstake");
-        require(user.lockedAtTimestamp <= 0, "can't unstake if you have locked");
-
-        require(_amount <= user.stakedAmount, "requires input _amount <= user.stakedAmount");
+        require(_amount <= userStakeAmountMap[_stakerAddr], "requires input _amount <= user.stakedAmount");
         SafeERC20.safeTransfer(IERC20(cardTokenAddr), _stakerAddr, _amount);
 
-        user.stakedAmount -= _amount;
+        userStakeAmountMap[_stakerAddr] -= _amount;
 
-        emit Unstaked(msg.sender, _stakerAddr, _amount, user.stakedAmount);
+        emit Unstaked(msg.sender, _stakerAddr, _amount, userStakeAmountMap[_stakerAddr]);
     }
 
-    function lock() external {
+    function lock(uint256 _amount) external {
+        UserTokenLock storage utl = userTokenLocks[msg.sender];
+
+        require(utl.lockedAtTimestamp <= 0 && utl.lockedAmount == 0, "user already has locked balance");
         
+        uint256 prevVaultTokenBalance = IERC20(cardTokenAddr).balanceOf(address(this));
+        SafeERC20.safeTransferFrom(IERC20(cardTokenAddr), address(msg.sender), address(this), _amount);
+        uint256 newVaultTokenBalance = IERC20(cardTokenAddr).balanceOf(address(this));
+        uint256 lockedAmount = newVaultTokenBalance - prevVaultTokenBalance;
+
+        utl.lockedAmount = lockedAmount;
+        utl.lockedAtTimestamp = now256();
+
+        emit Locked(msg.sender, _amount, lockedAmount);
     }
 
-    function claimUnlockedTokens() public {
-        User storage user = users[msg.sender];
+    function claimUnlockedTokens() external {
+        UserTokenLock storage utl = userTokenLocks[msg.sender];
 
+        require(utl.lockedAmount > 0, "no locked tokens");
+
+
+        // determine months since initial lock
+        uint256 timeElapsed = now256() - utl.lockedAtTimestamp;
+        uint monthsElapsed = timeElapsed / 30 days;
+        uint newClaimAmount;
+        if (monthsElapsed >= 12) {
+            // unlock all tokens and transfer out
+            // TODO also test wjat jap[[emns when i call claim twice for this block of code
+            newClaimAmount = utl.lockedAmount - utl.claimedAmount;
+        } else {
+            uint totalUnlockedTokens = utl.lockedAmount / monthsElapsed * 12;
+            newClaimAmount = totalUnlockedTokens - utl.claimedAmount;
+        }
+        SafeERC20.safeTransfer(IERC20(cardTokenAddr), msg.sender, newClaimAmount);
+
+        utl.claimedAmount += newClaimAmount;
+
+        emit ClaimedUnlocked(msg.sender, newClaimAmount, monthsElapsed, utl.claimedAmount);
     }
 
 
